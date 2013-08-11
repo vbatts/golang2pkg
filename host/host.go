b0b69eb6bcc53c3924ea7eff428d2c5656358ce8
Vincent Batts <vbatts@hashbangbash.com>
2013-08-11 11:28:15 -0400

    adding host command checks

---
+++ host/host.go
@@ -0,0 +1,66 @@
+/*
+For host type queries, like HasRpm, HasDpkg, HasDhMake, and HasRpmBuild
+(or similar).
+
+This will be used for determining how far you can go with making a build
+artifact on a box that does not have all the tools needed.
+*/
+package host
+
+import (
+	"os/exec"
+)
+
+//var DefaultRoot = "/"
+
+/*
+Searches PATH for cmd, and returns the path to it.
+Empty string is lack of presence.
+*/
+func Command(cmd string) string {
+	path, err := exec.LookPath(cmd)
+	if err != nil {
+		return ""
+	}
+	return path
+}
+
+// Whether rpmbuild is in PATH
+func HasRpmBuild() bool {
+	return len(Command("rpmbuild")) > 0
+}
+
+// Path to rpmbuild (if presence)
+func RpmBuild() string {
+	return Command("rpmbuild")
+}
+
+// Whether rpm is in PATH
+func HasRpm() bool {
+	return len(Command("rpm")) > 0
+}
+
+// Path to rpm (if presence)
+func Rpm() string {
+	return Command("rpm")
+}
+
+// Whether dpkg is in PATH
+func HasDpkg() bool {
+	return len(Command("dpkg")) > 0
+}
+
+// Path to dpkg (if presence)
+func Dpkg() string {
+	return Command("dpkg")
+}
+
+// Whether dh_make is in PATH
+func HasDhMake() bool {
+	return len(Command("dh_make")) > 0
+}
+
+// Path to dh_make (if presence)
+func DhMake() string {
+	return Command("dh_make")
+}
