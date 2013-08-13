03b90d91a103b59b7a602a3590d7676aaf43bdec
Vincent Batts <vbatts@hashbangbash.com>
2013-08-12 20:26:25 -0400

    setting up a tmp GOPATH

--- examples/imports.go
+++ examples/imports.go
@@ -5,8 +5,45 @@ import (
 	"go/build"
 	"go/parser"
 	"go/token"
+	"os"
+	"os/exec"
+	"path"
 	"strings"
 )
+
+func main() {
+	if len(os.Args[1:]) == 0 {
+		fmt.Fprintf(os.Stderr, "ERROR: provide package\n")
+		os.Exit(1)
+	}
+	t_path := path.Join(os.TempDir(), "golang2pkg-asdfasdf")
+	err := os.Mkdir(t_path, 0755)
+	if err != nil && os.IsExist(err) {
+		fmt.Fprintf(os.Stderr, "Re-using directory '%s'\n", t_path)
+  } else if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+		os.Exit(1)
+	}
+	//defer os.RemoveAll(t_path)
+
+	err = os.Setenv("GOPATH", t_path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+		os.Exit(1)
+	}
+
+	cmd := exec.Command("go", "get", "-d", os.Args[1])
+	fmt.Printf("%#v\n", cmd)
+	buf, err := cmd.Output()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: %s\n", err)
+		os.Exit(1)
+	}
+  if len(buf) > 0 {
+	  fmt.Printf("%s\n", buf)
+  }
+
+}
 
 var src = `
 package hurp
@@ -21,7 +58,7 @@ import (
 var c = 0
 `
 
-func main() {
+func demo() {
 	fset := token.NewFileSet() // positions are relative to fset
 	f, err := parser.ParseFile(fset, "src.go", src, 0)
 	if err != nil {
