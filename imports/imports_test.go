303a9cfb76197fddef3842fd2107d697d25ea2da
Vincent Batts <vbatts@hashbangbash.com>
2013-08-18 17:20:18 -0400

    checking that the two imports finders return match somewhat

--- imports/imports_test.go
+++ imports/imports_test.go
@@ -1,21 +1,27 @@
 package imports
 
 import (
-	"fmt"
 	"go/build"
 	"testing"
 )
 
 func TestFind(t *testing.T) {
+	def_pkgs := Imports{}
 	for _, src := range build.Default.SrcDirs() {
 		i, e := FindImports(src)
 		if e != nil {
 			t.Fatal(e)
 		}
-		//fmt.Printf("%#v\n", i)
-		fmt.Println(src)
-		for _, pkg := range i {
-			fmt.Println(pkg.ImportPath)
-		}
+		def_pkgs = append(def_pkgs, i...)
+	}
+
+	i, e := FindImportsDefault()
+	if e != nil {
+		t.Fatal(e)
+	}
+	if len(i) != len(def_pkgs) {
+		t.Errorf("FindImports of SrcDirs returned [%d], but FindImportsDefault returned [%d]",
+			len(def_pkgs),
+			len(i))
 	}
 }
