4c77213ac2454a2137bea1aabd6058b73c040de7
Vincent Batts <vbatts@hashbangbash.com>
2013-08-21 08:28:41 -0400

    Versioner handles git has now

--- version/version.go
+++ version/version.go
@@ -4,9 +4,12 @@ helpers to get the version information for a checked out library
 package version
 
 import (
+	"bytes"
 	"go/build"
+	"io/ioutil"
 	"os"
 	"path"
+	"strings"
 )
 
 type Version struct {
@@ -22,38 +25,88 @@ func (v Version) String() string {
 		return v.Value
 	}
 
-	if len(v.Path) > 0 {
-		// XXX call osutil for the git/hg/bzr version formating
+	// if there is a path and a VCS for that path, then call the corresponding func
+	if len(v.Path) > 0 && len(hasVcsDir(v.Path)) > 0 {
+		return VCSDirs[hasVcsDir(v.Path)](v.Path)
 	}
 
 	return "0.0"
 }
 
-var VCSDirs = []string{".git", ".bzr", ".hg"}
+/*
+Version control versioner
+
+Takes an argument of the path to check, and returns a string of the version
+*/
+type VcsFn func(string) string
+
+/*
+map of Version control directory names, and the func to call on
+a path of that type, in order to get a version.
+*/
+var VCSDirs = map[string]VcsFn{
+	".git": GitHead,
+	".bzr": nothing,
+	".hg":  nothing,
+}
+
+// XXX placeholder for actual vcs functions
+func nothing(p string) string { return p }
 
 /*
 Given a build.Package, check the Dir for a directory matching CVSDirRe,
 if nothing check parents until SrcRoot
 */
 func FromPackage(pkg *build.Package) Version {
-	pth := pkg.Dir
+	return FromDir(pkg.Dir, pkg.SrcRoot)
+}
+
+/*
+Check for VCS in pth file path, not to exceed root.
+*/
+func FromDir(pth, root string) Version {
 	for {
-		if hasVcsDir(pth) {
+		if len(hasVcsDir(pth)) > 0 {
 			return Version{Path: pth}
 		}
 		pth = path.Dir(pth)
-		if pth == pkg.SrcRoot {
+		if pth == root {
 			break
 		}
 	}
 	return Version{}
 }
 
-func hasVcsDir(pth string) bool {
-	for _, vcs := range VCSDirs {
+func hasVcsDir(pth string) string {
+	for vcs, _ := range VCSDirs {
 		if fi, err := os.Stat(path.Join(pth, vcs)); err == nil && fi.IsDir() {
-			return true
+			return vcs
 		}
 	}
-	return false
+	return ""
+}
+
+/*
+functionally equivalent to `git rev-parse --short HEAD`
+*/
+func GitHead(pth string) (hash string) {
+	file, err := os.Open(path.Join(pth, ".git", "HEAD"))
+	if err != nil {
+		return ""
+	}
+	defer file.Close()
+	buf, err := ioutil.ReadAll(file)
+	if err != nil {
+		return ""
+	}
+	ref_path := strings.TrimPrefix(strings.Trim(bytes.NewBuffer(buf).String(), "\n "), "ref: ")
+	ref_file, err := os.Open(path.Join(pth, ".git", ref_path))
+	if err != nil {
+		return ""
+	}
+	ref_buf, err := ioutil.ReadAll(ref_file)
+	if err != nil {
+		return ""
+	}
+	return strings.Trim(bytes.NewBuffer(ref_buf).String(), "\n ")[0:7]
 }
--- version/version_test.go
+++ version/version_test.go
@@ -1,7 +1,8 @@
 package version
 
 import (
-	//"fmt"
+	"fmt"
+	"path/filepath"
 	"testing"
 )
 
@@ -11,4 +12,12 @@ func TestBasics(t *testing.T) {
 	if v.String() != "0.0" {
 		t.Errorf("Empty version did not match '0.0', but returned %s", v.String())
 	}
+
+	b, err := filepath.Abs("..")
+	if err != nil {
+		t.Fatalf("error getting Abs path: %s", err)
+	}
+	v = FromDir(b, filepath.Dir(b))
+	fmt.Println(v)
+
 }
